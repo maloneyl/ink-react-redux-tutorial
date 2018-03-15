781b5008883ab2a6c36c2b74b02ae5f6250c1279
Maloney L <maloneyl@users.noreply.github.com>
2018-03-15 20:52:38 +0000

    end of tutorial

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import Scene from "./Scene";
+import Choices from "./Choices";
+import Story from "./Story";
+import { makeChoice } from "./state/actions";
+import "./App.css";
+
+const App = props =>
+  props.ending ? (
+    <div className="ending">🎉🎉 YOU WIN! 🎉🎉</div>
+  ) : (
+    <div className="App">
+      <Scene tags={props.tags} />
+      <Story sceneText={props.sceneText} />
+      <Choices choices={props.currentChoices} makeChoice={props.makeChoice} />
+    </div>
+  );
+
+const stateToProps = state => ({
+  tags: state.tags,
+  currentChoices: state.currentChoices,
+  sceneText: state.sceneText,
+  ending: state.ending
+});
+
+const dispatchToProps = dispatch => ({
+  makeChoice: idx => dispatch(makeChoice(idx))
+});
+
+export default connect(stateToProps, dispatchToProps)(App);
